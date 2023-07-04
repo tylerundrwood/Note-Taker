50458289da489ac0c84c758818322b6d63618f27
Tyler Underwood <twood9600@yahoo.com>
2023-07-04 09:20:40 -0500

    created public page

--- public/assets/js/note.js
+++ public/assets/js/note.js
@@ -12,3 +12,30 @@ const PORT = 5501
 
 // express function
 const app = express()
+
+// read json data
+app.use(express.json())
+
+// url
+app.use(express.urlencoded({ extended: true}))
+
+const readFromFile = util.promisify(fs.readFile);
+
+// gain access to public folder
+app.use(express.static('./public'));
+
+function browserRefresh(filePath) {
+  var html = fs.readFileSync(filePath);
+  var $ = cheerio.load(html);
+  $('body').append(`<script src="${process.env.BROWSER_REFRESH_URL}"></script>`);
+  return $.html();
+}
+// reflects the home page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+// reflects the notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
