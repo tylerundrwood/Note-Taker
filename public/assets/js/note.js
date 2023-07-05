71e4a85ce0d0a0e10899fb1e8b16f3b1b40a84e6
Tyler Underwood <twood9600@yahoo.com>
2023-07-05 13:19:23 -0500

    finished notejs

--- public/assets/js/note.js
+++ public/assets/js/note.js
@@ -57,3 +57,44 @@ function browserRefresh() {
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
+
+  const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+  
+  // post api notes
+  app.post('/api/notes', (req, res) => {
+    // Let the client know that their POST request was received
+    res.json(`${req.method} request received`);
+    
+      // deconstruct assignment for the req.body
+      const { title, text } = req.body;
+      // review object
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+          title,
+          text,
+          noteId: uniqid()
+        };
+    
+        readAndAppend(newNote, '../db/db.json');
+        console.log(`Note added successfully 🚀`);
+      } else {
+        res.error('Error in adding note');
+      }
+  })
+  
+  
+  // server listen
+  app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`)
+  })
