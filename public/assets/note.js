984a326b4c0ea1a114e397b11ce55c66bbf180ba
Tyler Underwood <twood9600@yahoo.com>
2023-07-06 18:27:17 -0400

    deployment issues fix

--- public/assets/note.js
+++ public/assets/note.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 // Route for index html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './index.html'))
 })
 
 // Route for notes html
